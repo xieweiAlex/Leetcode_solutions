9004ef67d727fa08a99e8b0ca2af2a045f1c0261
xieweialex <xieweialex@163.com>
2017-02-16 18:28:11 -0600

    third day

---
+++ ForestAcademy/DeleteDuplicates.java
@@ -0,0 +1,53 @@
+package ForestAcademy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Alex_Xie on 15/02/2017.
+
+ Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+ For example,
+ Given 1->1->2, return 1->2.
+ Given 1->1->2->3->3, return 1->2->3.
+
+ https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class DeleteDuplicates {
+
+    class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+    }
+
+
+    public ListNode deleteDuplicates1(ListNode head) {
+        if(head == null || head.next == null)return head;
+        head.next = deleteDuplicates(head.next);
+        return head.val == head.next.val ? head.next : head;
+    }
+
+    // doesn't work!
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null) return null;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        ListNode tempNode = head.next;
+        while (tempNode != null) {
+            Integer val = head.next.val;
+            if (map.containsKey(val)) {
+                tempNode = tempNode.next.next;
+            } else {
+                tempNode = tempNode.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+
+
+
---
+++ ForestAcademy/HammingWeight.java
@@ -0,0 +1,46 @@
+package ForestAcademy;
+
+/**
+ * Created by Alex_Xie on 15/02/2017.
+
+ Write a function that takes an unsigned integer and returns the number of ’1' bits it has (also known as the Hamming weight).
+
+ For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, so the function should return 3.
+
+ https://leetcode.com/problems/number-of-1-bits/
+
+ */
+public class HammingWeight {
+
+    // 17
+    // 17,8,1   8,4,0   4,2,0   2,0,1
+    public int hammingWeight(int n) {
+
+        int res = 0;
+        while (n >= 1) {
+            if (n % 2 != 0) {
+                res++;
+            }
+            n /= 2;
+        }
+        return res;
+    }
+
+    public static int hammingWeight1(Integer n) {
+        int ones = 0;
+        while(n!=0) {
+            ones = ones + (n & 1);
+            n = n>>>1;
+        }
+        return ones;
+    }
+
+    public static void main(String[] args) {
+        HammingWeight obj = new HammingWeight();
+        System.out.println(obj.hammingWeight(11));
+        System.out.println(obj.hammingWeight(8));
+        System.out.println(obj.hammingWeight(2));
+    }
+
+
+}
