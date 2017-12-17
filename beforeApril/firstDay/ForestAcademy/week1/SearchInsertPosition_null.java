495677847c5e709a07337d367e9aece678a54126
Alex Xie <xieweialex@gmail.com>
2017-12-17 12:25:58 -0800

    Dec 17th

--- beforeApril/firstDay/ForestAcademy/week1/SearchInsertPosition_null.java
+++ beforeApril/firstDay/ForestAcademy/week1/SearchInsertPosition_null.java
@@ -32,6 +32,32 @@ public class SearchInsertPosition_null {
     }
 
 
+    // find the first position >= target
+    public int searchInsert1(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low + 1 < high) {
+            int middle = low + (high - low) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                low = middle;
+            } else {
+                high = middle;
+            }
+        }
+
+        if (nums[low] >= target) {
+            return low;
+        }
+        if (nums[high] >= target) {
+            return high;
+        }
+
+        return high + 1;
+    }
+
+
    public static void main(String[] args) {
        SearchInsertPosition_null obj = new SearchInsertPosition_null();
        System.out.println(obj.searchInsert(new int[]{1,3,5,6}, 5));
